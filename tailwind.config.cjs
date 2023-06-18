c91454e5230731c23a455b01d361847c9c270967
JayeshVP24 <jayeshpotlabattini@gmail.com>
2023-06-19 00:41:25 +0530

    SETUP DONE

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Inter", ...defaultTheme.fontFamily.sans],
+				rubik: ["Rubik", ...defaultTheme.fontFamily.serif],
+			},
+		},
+	},
+	plugins: [],
+}
