2b3101a32183120d3f4c6ab53fbd1ab49583b19c
Horror26 <1032210438@tcetmumbai.in>
2023-06-21 19:24:03 +0530

    file changed

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,5 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
